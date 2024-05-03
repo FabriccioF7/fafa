9f08562569122c6c07d7acabd8146d5da8f11a03
FabriccioF7 <FabriccioF7@users.noreply.github.com>
2024-05-03 19:06:58 +0000

    ✅ Ready to clone and code.

--- fafa/ext/commands.py
+++ fafa/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from fafa.ext.auth import create_user
+from fafa.ext.database import db
+from fafa.models import Product
 
 
 def create_db():
